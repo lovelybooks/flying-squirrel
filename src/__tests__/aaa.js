4033a106863c63db663f778adb04277cacee165d
Mikolaj Dadela <mikolaj.dadela@hgv-online.de>
2015-03-20 16:27:30 +0100

    Reported a bug and added a workaround for buggy Jasmine timer.

---
+++ src/__tests__/aaa.js
@@ -0,0 +1,10 @@
+/*jshint jasmine: true */
+'use strict';
+
+// See https://github.com/jasmine/jasmine/issues/809
+describe('a', function () {});
+describe('b', function () {});
+describe('c', function () {});
+describe('d', function () {});
+describe('e', function () {});
+describe('f', function () {});
